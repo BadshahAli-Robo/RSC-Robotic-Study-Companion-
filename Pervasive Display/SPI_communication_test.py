69072454ccb85f732b59b57c87c1ae58cffa6fb9
Ali Badshah <114699457+BadshahAli-Robo@users.noreply.github.com>
2025-01-15 16:59:53 +0200

    Updated SPI Communication

--- Pervasive Display/SPI_communication_test.py
+++ Pervasive Display/SPI_communication_test.py
@@ -8,3 +8,14 @@ try:
     print("SPI communication successful.")
 except Exception as e:
     print("SPI test failed:", e)
+
+
+
+
+# just for knowledge
+# When you're using an e-paper display with SPI communication:
+# 
+# Master (RPi Pico) sends commands and pixel data to the display via MOSI.
+# The display might send status information (e.g., "I'm busy") back to the Pico via MISO.
+# Communication is synchronized by the clock signal (SCK), which the Pico generates.
+# The CS pin ensures that the correct device on the SPI bus is active during communication.
